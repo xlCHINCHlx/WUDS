a903eddd0b0365f92d02b9089ceeeb1d20a6dc76
xlCHINCHlx <n65ghz@gmail.com>
2016-07-08 17:45:33 -0700

    Cleaning things upp a bit. . .

--- run/run.py
+++ run/run.py
@@ -1,6 +1,5 @@
 from subprocess import call
-call (["docker", "build", "-t", "chinch/wuds1", "/home/wtmf/dock/kali-wuds"])
 
-call (["docker", "run", "-ti", "--privileged", "--net=host", "-v", "/dev/bus/usb:/dev/bus/usb", "chinch/wuds1"])
+call (["docker", "run", "-ti", "--privileged", "--net=host", "-v", "/dev/bus/usb:/dev/bus/usb", "chinch/WUDS"])
 
 
