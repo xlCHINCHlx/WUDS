9e93b554252885d72fa728ac6dfda9c630778a75
xlCHINCHlx <n65ghz@gmail.com>
2016-07-08 18:32:10 -0700

    cleaning up text

--- run/run.py
+++ run/run.py
@@ -1,5 +1,5 @@
 from subprocess import call
 
-call (["docker", "run", "-ti", "--privileged", "--net=host", "-v", "/dev/bus/usb:/dev/bus/usb", "chinch/WUDS"])
+call (["docker", "run", "-ti", "--privileged", "--net=host", "-v", "/dev/bus/usb:/dev/bus/usb", "chinch/wuds"])
 
 
